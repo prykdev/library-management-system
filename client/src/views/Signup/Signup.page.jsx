835c87aeacedc2d650df2e7f39464c74d6f01d3f
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2021-12-03 16:02:20 +0530

    added style

--- client/src/views/Signup/Signup.page.jsx
+++ client/src/views/Signup/Signup.page.jsx
@@ -16,38 +16,55 @@ export const Signup = () => {
     return (
 
 
-        <div>
-            <label>Name</label>
-              <br />
-              <input type="name"
-                placeholder="Your Name"
-                onChange={e => setName(e.target.value)}
-              />
-              <br />
-              <label>Enter Email</label>
-              <br />
-              <input type="email"
-                placeholder="Your email"
-                onChange={e => setEmail(e.target.value)}
-                required
-              />
-              <br />
-              <label>Enter Password</label>
-              <br />
-              <input
-                type="password"
-                placeholder="At least 8 Characters"
-                onChange={e => setPassword(e.target.value)}
-                required
-              />
-              <br />
-              <br />
-              <input type="checkbox" required />
-              <label>
-                I agree to the terms of service
-              </label>
-              <br />
-              <button type='summit' onClick={clicked}>Signup</button>
+        <div className="back">
+          <div className="text">
+                <h1>Welcome to the family</h1>
+                <p>let’s get you all set up so you can verify your4 personal account and begin settihng up your profile. </p>
+            </div>
+            <div className="Mid">
+
+                <div className="image">
+                 
+                  
+                </div>
+                <div className="form">
+
+                    <label>Name</label>
+                    <br />
+                    <input type="name"
+                      placeholder="Your Name"
+                      onChange={e => setName(e.target.value)}
+                    />
+                    <br />
+                    <label>Enter Email</label>
+                    <br />
+                    <input type="email"
+                      placeholder="Your email"
+                      onChange={e => setEmail(e.target.value)}
+                      required
+                    />
+                    <br />
+                    <label>Enter Password</label>
+                    <br />
+                    <input
+                      type="password"
+                      placeholder="At least 8 Characters"
+                      onChange={e => setPassword(e.target.value)}
+                      required
+                    />
+                    <br />
+                    <br />
+                    <div>
+
+                      <input type="checkbox" required />
+                      <label>
+                        I agree to the terms of service
+                      </label>
+                    </div>
+                    <br />
+                    <button  className ="btn"type='summit' onClick={clicked}>Create account</button>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/views/login/Login.page.jsx
+++ client/src/views/login/Login.page.jsx
@@ -1,5 +1,6 @@
 import React ,{useState} from 'react'
 import {useNavigate} from "react-router-dom"
+import '../../components/Style/Signup.style.css'
 export const Login = () => {
     const navigate = useNavigate()
     // eslint-disable-next-line
@@ -11,6 +12,13 @@ export const Login = () => {
     }
     return (
         <div>
+            <div className="text">
+                <h1>Welcome to the family</h1>
+                <p>let’s get you all set up so you can verify your4 personal account and begin settihng up your profile. </p>
+            </div>
+            <div className="image">
+                <img src="../../layout/img.png" alt=""/>
+            </div>
             <div className='form'>
                 <label >Enter Email</label><br/>
                 <input 
